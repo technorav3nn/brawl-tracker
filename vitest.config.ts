b1221f26f0dafacc8dd08400029040584c1f1499
technorav3nn <Colin01460@outlook.com>
2023-11-22 09:39:15 -0500

    chore: add coverage to ci

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	test: {
+		exclude: ["**/node_modules", "**/dist", ".idea", ".git", ".cache"],
+		passWithNoTests: true,
+		coverage: {
+			enabled: true,
+			reporter: ["text", "json-summary", "json"],
+			include: ["src"],
+			exclude: [
+				// All ts files that only contain types, due to ALL
+				"**/*.{interface,type,d}.ts",
+				"**/{interfaces,types}/*.ts",
+				// All index files that *should* only contain exports from other files
+				"**/index.{js,ts}",
+				// All exports files that make subpackages available as submodules
+				"**/exports/*.{js,ts}",
+			],
+		},
+	},
+});
