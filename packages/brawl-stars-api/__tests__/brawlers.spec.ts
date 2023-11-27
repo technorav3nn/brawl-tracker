5cea5ca032b78b812af6b5ff1c622c67e08c155e
technorav3nn <Colin01460@outlook.com>
2023-11-27 13:10:27 -0500

    test: add getBrawler test

--- packages/brawl-stars-api/__tests__/brawlers.spec.ts
+++ packages/brawl-stars-api/__tests__/brawlers.spec.ts
@@ -2,12 +2,20 @@ import { test, describe, expect } from "vitest";
 import { client } from "./util/client";
 
 describe("BrawlersApi", () => {
-	test("GIVEN all brawlers THEN should contain Charlie", async () => {
-		const brawlers = await client.brawlers.getBrawlers();
-		expect(brawlers).toContainEqual(expect.objectContaining({ name: "CHARLIE" }));
+	describe("getBrawlers", () => {
+		test("GIVEN all brawlers THEN should contain Charlie", async () => {
+			const brawlers = await client.brawlers.getBrawlers();
+			expect(brawlers).toContainEqual(expect.objectContaining({ name: "CHARLIE" }));
+		});
+		test("GIVEN all brawlers with limit THEN should contain 5 brawlers", async () => {
+			const brawlers = await client.brawlers.getBrawlers(5);
+			expect(brawlers).toHaveLength(5);
+		});
 	});
-	test("GIVEN all brawlers with limit THEN should contain 5 brawlers", async () => {
-		const brawlers = await client.brawlers.getBrawlers(5);
-		expect(brawlers).toHaveLength(5);
+
+	describe("getBrawler", () => {
+		test("GIVEN a brawler id THEN should return the brawler", async () => {
+			await expect(client.brawlers.getBrawler(16000000)).resolves.toHaveProperty("name", "SHELLY");
+		});
 	});
 });
--- packages/brawl-stars-api/__tests__/clubs.spec.ts
+++ packages/brawl-stars-api/__tests__/clubs.spec.ts
@@ -3,12 +3,12 @@ import { client } from "./util/client";
 
 describe("ClubsApi", () => {
 	describe("getClub", () => {
-		test("WITH club tag THEN should be a valid club", async () => {
+		test("WITH valid club tag THEN should be a valid club", async () => {
 			const club = await client.clubs.getClub("#2PP00");
 			expect(club.tag).toEqual("#2PP00");
 		});
 
-		test("WITH bad club tag THEN should throw a 404", async () => {
+		test("WITH invalid club tag THEN should throw a 404", async () => {
 			const club = client.clubs.getClub("#DOESNOTEXIST");
 			await expect(club).rejects.toThrow("404");
 		});
