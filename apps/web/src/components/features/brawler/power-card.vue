b2ad2c9dd9ee9ba77b0554f4b165c0d1080de7f4
technorav3nn <Colin01460@outlook.com>
2023-12-29 15:59:15 -0500

    fix(web): power-card style

--- apps/web/src/components/features/brawler/power-card.vue
+++ apps/web/src/components/features/brawler/power-card.vue
@@ -11,12 +11,12 @@ defineProps<{
 	<div class="mt-3 grid grid-cols-1 bg-card lg:grid-cols-2">
 		<div v-for="power in $props.powers" :key="power.name" class="bg-card p-4 shadow">
 			<div class="flex flex-row gap-4 lg:flex-col">
-				<div class="flex flex-row items-center gap-3 max-lg:max-h-[60px] max-lg:w-full max-lg:max-w-[60px]">
+				<div class="flex max-h-[50px] w-full max-w-[50px] flex-row items-center gap-3">
 					<NuxtImg
 						:src="power.imageUrl"
-						class="aspect-square max-h-[60px] w-full max-w-[60px] self-center bg-contain object-cover lg:self-auto"
-						width="60"
-						height="60"
+						class="aspect-square max-h-[50px] w-full max-w-[50px] self-center bg-contain object-cover lg:self-auto"
+						width="50"
+						height="50"
 					/>
 					<p class="hidden text-xl font-bold tracking-tight lg:block">{{ power.name }}</p>
 				</div>
--- apps/web/src/layouts/default.vue
+++ apps/web/src/layouts/default.vue
@@ -7,7 +7,7 @@ defineOptions({ inheritAttrs: false });
 		<SectionsHeader />
 		<div class="mt-24 flex-auto justify-between">
 			<div class="w-full">
-				<main class="mx-auto px-5 pb-10 md:w-5/6 md:px-0" v-bind="$attrs">
+				<main class="mx-auto px-5 pb-10 md:w-[78%] md:px-0" v-bind="$attrs">
 					<slot />
 				</main>
 			</div>
