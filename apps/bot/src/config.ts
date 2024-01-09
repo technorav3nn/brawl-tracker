cddc40ca1e113d23378d387e6a17ec2d35f21a8d
technorav3nn <Colin01460@outlook.com>
2024-01-08 23:51:14 -0500

    fix: lint-staged

--- apps/bot/src/config.ts
+++ apps/bot/src/config.ts
@@ -4,5 +4,5 @@ import { setup } from "@skyra/env-utilities";
 
 export const mainFolder = getRootData().root;
 export const rootFolder = join(mainFolder, "..");
-const a = 1;
+
 setup(join(rootFolder, ".env"));
