d3322a09f8cc1b90d7abcfa1d685394a77235fe3
technorav3nn <Colin01460@outlook.com>
2024-02-26 23:09:45 -0500

    fix(test): finally fixed coverage errors

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,22 +5,12 @@ import GithubActionsReporter from "vitest-github-actions-reporter";
 export const configShared = defineConfig({
 	test: {
 		reporters: process.env.GITHUB_ACTIONS ? ["default", new GithubActionsReporter()] : "default",
-		exclude: ["**/node_modules", "**/dist", ".idea", ".git", ".cache", ".yarn", "apps"],
+		exclude: ["**/node_modules", "**/dist", ".idea", ".git", ".cache", ".yarn", "apps", "src", "*.get.ts"],
+		include: ["__tests__/**/*.{spec,test}.{js,ts}"],
 		passWithNoTests: true,
 		coverage: {
 			enabled: true,
-			reporter: ["text", "json-summary", "json", "html"],
-			exclude: [
-				// All ts files that only contain types, due to ALL
-				"**/*.{interface,type,d}.ts",
-				"**/{interfaces,types}/*.ts",
-				// All index files that *should* only contain exports from other files
-				"**/index.{js,ts}",
-				// All exports files that make subpackages available as submodules
-				"**/exports/*.{js,ts}",
-				// Config files
-				"**/**{config}.{js,ts}",
-			],
+			reporter: ["json-summary", "json"],
 		},
 	},
 });
