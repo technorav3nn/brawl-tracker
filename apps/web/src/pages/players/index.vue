b120308802393dacb41834b41fa14e2c78c44212
technorav3nn <Colin01460@outlook.com>
2024-05-30 14:07:19 -0400

    chore: update yarn version

---
+++ apps/web/src/pages/players/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="flex flex-row justify-between pb-8">
+		<div>
+			<h1 class="text-6xl font-bold leading-tight tracking-tight text-foreground">Players</h1>
+			<p class="text-sm font-medium text-muted-foreground">
+				Search for a players profile with their unique tag!
+			</p>
+		</div>
+		<NuxtImg
+			src="https://supercell.com/images/e93a34598d3723641a72eb9ce02691f6/games_thumbnail_brawlstars.5cd76330.jpg"
+			alt="Brawl Stars"
+			width="100"
+			format="webp"
+		/>
+	</div>
+</template>
