8ecc6b8f61b37c52d31056dc8595411aece1da4d
technorav3nn <Colin01460@outlook.com>
2024-09-15 21:46:16 -0400

    fix(test): json-summary reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config";
 export const configShared = defineConfig({
 	test: {
 		reporters: process.env.GITHUB_ACTIONS
-			? ["verbose", "github-actions", "hanging-process"]
+			? ["json", "verbose", "github-actions", "hanging-process"]
 			: ["verbose", "hanging-process", "html"],
 		exclude: ["**/node_modules", "**/dist", ".idea", ".git", ".cache", ".yarn", "apps", "src", "*.get.ts"],
 		include: ["__tests__/**/*.{spec,test}.{js,ts}"],
