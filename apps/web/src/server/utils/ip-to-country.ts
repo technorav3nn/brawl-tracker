a850b531f470a8ef587fa7a120b3f2f43fc9d3f9
technorav3nn <Colin01460@outlook.com>
2025-01-20 17:08:45 -0500

    fix(deps): remove geoip-country

--- apps/web/src/server/utils/ip-to-country.ts
+++ apps/web/src/server/utils/ip-to-country.ts
@@ -1,4 +1,4 @@
-import geoip from "geoip-country";
+// import geoip from "geoip-country";
 
 export function useIpToCountry(ip: string) {
 	const event = useEvent();
@@ -8,17 +8,20 @@ export function useIpToCountry(ip: string) {
 	}
 
 	// Check if its localhost, since the browser doesn't send the IP in development
+	// eslint-disable-next-line sonarjs/no-all-duplicated-branches
 	if (ip === "127.0.0.1") {
+		return "US";
+	} else {
 		return "US";
 	}
 
-	const result = geoip.lookup(ip);
-	if (!result) {
-		throw createError({
-			statusMessage: "Failed to get IP location",
-			statusCode: 500,
-		});
-	}
+	// const result = geoip.lookup(ip);
+	// if (!result) {
+	// 	throw createError({
+	// 		statusMessage: "Failed to get IP location",
+	// 		statusCode: 500,
+	// 	});
+	// }
 
-	return result.country;
+	// return result.country;
 }
